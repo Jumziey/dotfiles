ec0c79638d93e5171867523e9a113271a033718f
Jesper Vesterberg <jumziey@gmail.com>
2020-03-17 16:29:14 +0100

    Added the create-links.sh script which actually creates the symlinks in the host system

---
+++ create-links.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ignores=(
+  "dot-funcs.sh"
+  "dot-funcs.bats"
+  "create-symlinks.sh"
+  ".gitignore"
+  ".nvimrc"
+  "arch-packages"
+)
+
+script_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+# shellcheck source=dot-funcs.sh
+source "$script_root/dot-funcs.sh"
+host_root="$HOME"
+
+backup_root="$script_root/backup"
+create_backup_root "$backup_root"
+
+dotfiles=$(
+  cd "$script_root" || exit
+  git ls-files
+)
+
+for repo_file in $dotfiles; do
+  #Making sure its not an ignored file
+  echo "$repo_file"
+  if ! ignored "$repo_file" "${ignores[@]}"; then
+    backup "$backup_root" "$host_root" "$repo_file"
+    link_dotfile "$script_root" "$host_root" "$repo_file"
+  fi
+done
