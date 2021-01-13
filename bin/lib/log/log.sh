57aa5f1b488269ac6380cdea078d8ad3e223ca1c
Jesper 'jumziey' Vesterberg <jumziey@gmail.com>
2021-01-13 23:33:22 +0100

    Added the concept of log level, which basically is a way to modify ones log state to relate to the previous messages.

--- bin/lib/log/log.sh
+++ bin/lib/log/log.sh
@@ -2,16 +2,43 @@
 
 log_success() {
 	text="$1"
-	echo -e "\e[32;1mSUCCESS: \e[0m${text}"
+	level=$(convert_string_number "$2")
+	local prefix
+	for _ in $(seq 1 1 "$level");do
+		prefix+="\t"
+	done
+
+	echo -e "$prefix\e[32;1mSUCCESS: \e[0m${text}"
 }
 
 log_fail() {
 	text="$1"
-	echo -e "\e[31;1mFAIL: \e[0m${text}"
+	level=$(convert_string_number "$2")
+	local prefix
+	for _ in $(seq 1 1 "$level");do
+		prefix+="\t"
+	done
+
+	echo -e "$prefix\e[31;1mFAIL: \e[0m${text}"
 }
 
 log_info() {
 	text="$1"
-	echo -e "\e[1;33;1mINFO: \e[0m${text}"
-
+	level=$(convert_string_number "$2")
+	local prefix
+	for _ in $(seq 1 1 "$level");do
+		prefix+="\t"
+	done
+	echo -e "$prefix\e[1;33;1mINFO: \e[0m${text}"
 }
+
+convert_string_number() {
+	string="$1"
+
+	if [[ "$string" ==  '' ]];then 
+			echo 0; 
+	else 
+		echo "$string"; 
+	fi
+}
+
