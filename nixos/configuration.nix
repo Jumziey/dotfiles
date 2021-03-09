77c68af3d66ef3c10e9244175f3a96848f24a93d
Jumziey <jumziey@gmail.com>
2021-03-09 21:46:13 +0100

    Added sshd service

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -168,5 +168,8 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.09";
+
+  services.sshd.enable = true;
+
 }
 
