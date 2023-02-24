0d92fc22ea41dad1c248d9862e0305ef9782dba6
Jumziey <jumziey@gmail.com>
2023-02-24 22:17:37 +0100

    Removed windows related link ps1 for vim, I would never develop on windows anymore, unless something really crazy happened

--- win-nvim-links.ps1
+++
@@ -1,6 +0,0 @@
-$systemNvim = "$HOME\AppData\Local\nvim"
-$repoNvim = "$PSScriptRoot\.config\nvim"
-
-echo "Creating Symbolic Link:"
-echo "$systemNvim -> $repoNvim"
-New-Item -Path $systemNvim -ItemType SymbolicLink -Value $repoNvim
