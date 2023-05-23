8fd9941fe7a701f593c26208803024e9f0c51d5d
Jumziey <jumziey@gmail.com>
2023-05-23 13:27:56 +0200

    Added the bitwarden-bws package to my default configuration

--- nixos/jumziLaptop/flake.nix
+++ nixos/jumziLaptop/flake.nix
@@ -2,20 +2,23 @@
   description = "JumziConf";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    jumzipkgs.url = "./pkgs/bitwarden-bws";
   };
 
-  outputs = { nixpkgs, ... }: 
+  outputs = { nixpkgs, jumzipkgs, ... }: 
     let 
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
         config = {allowUnfree = true;};
       };
+      jpkgs = jumzipkgs.packages.${system};
       lib = nixpkgs.lib;
     in {
       nixosConfigurations = {
         jumziLaptop = lib.nixosSystem {
           inherit system;
+          specialArgs = {inherit jpkgs;};
           modules = [
             ./configuration.nix 
           ];
